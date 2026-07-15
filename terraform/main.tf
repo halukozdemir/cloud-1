4095a0b24a305cfb37b16faec8aedbac6577b830
Haluk Özdemir <0halukozdemir@gmail.com>
2026-07-15 17:39:15 +0300

    terraform: sunucunun kaynakları tanımlandı

--- terraform/main.tf
+++ terraform/main.tf
@@ -26,39 +26,55 @@ resource "aws_key_pair" "cloud1" {
   public_key = file(pathexpand("~/.ssh/cloud-1.pub"))
 }
 
-resource "aws_security_group" "web"{
-    name = "cloud-1-web"
-    description = "Allow SSH, HTTP and HTTPS from anywhere"
+resource "aws_security_group" "web" {
+  name        = "cloud-1-web"
+  description = "Allow SSH, HTTP and HTTPS from anywhere"
 
-    ingress {
-        description = "SSH"
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-    ingress {
-        description = "HTTP"
-        from_port = 80
-        to_port = 80
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-    
-    ingress {
-      description = "HTTPS"
-      from_port = 443
-      to_port = 443
-      protocol = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-    }
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-    egress {
-        description = "All outbound"
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "All outbound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "server" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t3.micro"
+  key_name               = aws_key_pair.cloud1.key_name
+  vpc_security_group_ids = [aws_security_group.web.id]
+
+  tags = {
+    Name = "cloud-1"
+  }
+}
+
+output "public_ip" {
+  value       = aws_instance.server.public_ip
+  description = "Server's public IP"
 }
